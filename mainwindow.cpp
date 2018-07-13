e2077e0054ffc7154270092ca7b11107c1d4d63a
RhodiColossus <zkfdrbxthybre@gmail.com>
2018-07-13 19:48:27 +0300

    добавил кординаты

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,15 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->pushLift_1->move(50,420);
+    ui->pushLift_2->move(150,420);
+    ui->pushLift_3->move(250,420);
+
     ui->pushLift_1->setFixedSize(35,50);
     ui->pushLift_1->setIcon(QIcon(":/new/prefix1/close"));
     ui->pushLift_1->setIconSize(QSize(35,50));
+
     ui->pushLift_2->setFixedSize(35,50);
     ui->pushLift_2->setIcon(QIcon(":/new/prefix1/close"));
     ui->pushLift_2->setIconSize(QSize(35,50));
+
     ui->pushLift_3->setFixedSize(35,50);
     ui->pushLift_3->setIcon(QIcon(":/new/prefix1/close"));
     ui->pushLift_3->setIconSize(QSize(35,50));
+
 
 }
 
